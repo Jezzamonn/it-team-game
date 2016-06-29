2943698d92e0f5eff70eb0a42a7c35ae1b34e2e0
65linesofcode <jchu0740@uni.sydney.edu.au>
2016-06-29 15:32:45 +1000

    test commit

---
+++ 😂.js
@@ -0,0 +1,9 @@
+var s = '😂';
+console.log(s[0]);
+console.log(s[1]);
+console.log(s.charCodeAt(0).toString(16));
+console.log(s.charCodeAt(1).toString(16));
+console.log(s.toString(16));
+console.log(s[0] === s[1])
+console.log(s[0] + s[1] === s)
+console.log(s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s)
