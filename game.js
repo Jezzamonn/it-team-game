3f264b4691ccf462ac6c91e9ab17fe22069d8cda
Jezzamonn <jezzamonn@gmail.com>
2016-06-30 00:01:46 +1000

    Add four different players

--- game.js
+++ game.js
@@ -20,28 +20,56 @@ function create() {
 }
 
 function initialisePlayers() {
-    // TODO: Make the number of players selectable somewher, somehow
-    players = [];
+    // TODO: Make the number of players selectable somewhere, somehow
     
-    // Create a new object, storing information for each player
-    players[0] = {
-        sprite: game.add.sprite(200, 200, 'player1'),
-        keys: game.input.keyboard.addKeys({
-            'up': Phaser.KeyCode.W,
-            'down': Phaser.KeyCode.S,
-            'left': Phaser.KeyCode.A,
-            'right': Phaser.KeyCode.D
-        })
-    }
-    players[1] = {
-        sprite: game.add.sprite(400, 400, 'player1'),
-        keys: game.input.keyboard.addKeys({
-            'up': Phaser.KeyCode.UP,
-            'down': Phaser.KeyCode.DOWN,
-            'left': Phaser.KeyCode.LEFT,
-            'right': Phaser.KeyCode.RIGHT
-        })
-    }
+    // This creates all the players with one big array literal.
+    // Could bit made a little more modular, but on the whole it's ok.
+    players = [
+    
+        // player 1
+        {
+            sprite: game.add.sprite(0, 0, 'player1'),
+            keys: game.input.keyboard.addKeys({
+                'up':    Phaser.KeyCode.UP,
+                'left':  Phaser.KeyCode.LEFT,
+                'down':  Phaser.KeyCode.DOWN,
+                'right': Phaser.KeyCode.RIGHT,
+            })
+        },
+    
+        // player 2
+        {
+            sprite: game.add.sprite(600, 0, 'player1'),
+            keys: game.input.keyboard.addKeys({
+                'up':    Phaser.KeyCode.W,
+                'left':  Phaser.KeyCode.A,
+                'down':  Phaser.KeyCode.S,
+                'right': Phaser.KeyCode.D,
+            })
+        },
+    
+        // player 3
+        {
+            sprite: game.add.sprite(0, 400, 'player1'),
+            keys: game.input.keyboard.addKeys({
+                'up':    Phaser.KeyCode.I,
+                'left':  Phaser.KeyCode.J,
+                'down':  Phaser.KeyCode.K,
+                'right': Phaser.KeyCode.L,
+            })
+        },
+    
+        // player 4
+        {
+            sprite: game.add.sprite(600, 400, 'player1'),
+            keys: game.input.keyboard.addKeys({
+                'up':    Phaser.KeyCode.G,
+                'left':  Phaser.KeyCode.V,
+                'down':  Phaser.KeyCode.B,
+                'right': Phaser.KeyCode.N,
+            })
+        },
+    ]
 }
 
 function update() {
