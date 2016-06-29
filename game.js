bc2d094f86ba3ccbb8e79d321fca0fcae39d46c0
Jezzamonn <jezzamonn@gmail.com>
2016-06-29 15:59:04 +1000

    Player input

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', { preload: preload, create: create });
 
+var player;
+var keys;
+
 function preload() {
 
     game.load.image('player1', 'p1.png');
+    
+    keys = game.input.keyboard.createCursorKeys();
 
 }
 
@@ -10,6 +15,21 @@ function create() {
 
     game.stage.backgroundColor = '#3433bb';
     //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
-    var test = game.add.sprite(200, 200, 'player1');
+    player = game.add.sprite(200, 200, 'player1');
 
+}
+
+function update() {
+    if (keys.down.isDown) {
+        player.y += 1;
+    }
+    if (keys.up.isDown) {
+        player.y -= 1;
+    }
+    if (keys.right.isDown) {
+        player.x += 1;
+    }
+    if (keys.left.isDown) {
+        player.x -= 1;
+    }
 }
