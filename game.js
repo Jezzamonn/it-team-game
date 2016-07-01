f2944b194efb14729ac7b5cd213bdb1d760fdf9e
Jezzamonn <jezzamonn@gmail.com>
2016-07-01 23:05:23 +1000

    Made player creation more modular, added boundaries and shrunk players

--- game.js
+++ game.js
@@ -25,68 +25,66 @@ function initialisePlayers() {
     // This creates all the players with one big array literal.
     // Could bit made a little more modular, but on the whole it's ok.
     players = [
-    
-        // player 1
-        {
-            sprite: game.add.sprite(0, 0, 'player1'),
-            keys: game.input.keyboard.addKeys({
-                'up':    Phaser.KeyCode.UP,
-                'left':  Phaser.KeyCode.LEFT,
-                'down':  Phaser.KeyCode.DOWN,
-                'right': Phaser.KeyCode.RIGHT,
-            })
-        },
-    
-        // player 2
-        {
-            sprite: game.add.sprite(600, 0, 'player1'),
-            keys: game.input.keyboard.addKeys({
-                'up':    Phaser.KeyCode.W,
-                'left':  Phaser.KeyCode.A,
-                'down':  Phaser.KeyCode.S,
-                'right': Phaser.KeyCode.D,
-            })
-        },
-    
-        // player 3
-        {
-            sprite: game.add.sprite(0, 400, 'player1'),
-            keys: game.input.keyboard.addKeys({
-                'up':    Phaser.KeyCode.I,
-                'left':  Phaser.KeyCode.J,
-                'down':  Phaser.KeyCode.K,
-                'right': Phaser.KeyCode.L,
-            })
-        },
-    
-        // player 4
-        {
-            sprite: game.add.sprite(600, 400, 'player1'),
-            keys: game.input.keyboard.addKeys({
-                'up':    Phaser.KeyCode.G,
-                'left':  Phaser.KeyCode.V,
-                'down':  Phaser.KeyCode.B,
-                'right': Phaser.KeyCode.N,
-            })
-        },
+        createPlayer('player1', ['UP', 'LEFT', 'DOWN', 'RIGHT']),
+        createPlayer('player1', ['W', 'A', 'S', 'D']),
+        createPlayer('player1', ['I', 'J', 'K', 'L']),
+        createPlayer('player1', ['G', 'V', 'B', 'N']),
     ]
+    
+    // Space out the players
+    players[1].sprite.x = game.width - players[1].sprite.width;
+    players[2].sprite.y = game.height - players[2].sprite.height;
+    players[3].sprite.x = game.width - players[3].sprite.width;
+    players[3].sprite.y = game.height - players[3].sprite.height;
+}
+
+function createPlayer(spriteName, keyNames) {
+    player = {
+        sprite: game.add.sprite(0, 0, 'player1'),
+        keys: game.input.keyboard.addKeys({
+            'up':    Phaser.KeyCode[keyNames[0]],
+            'left':  Phaser.KeyCode[keyNames[1]],
+            'down':  Phaser.KeyCode[keyNames[2]],
+            'right': Phaser.KeyCode[keyNames[3]],
+        })
+    }
+    player.sprite.scale.setTo(0.2, 0.2)
+    return player;
 }
 
 function update() {
     for (var i = 0; i < players.length; i ++) {
         sprite = players[i].sprite;
         keys = players[i].keys;
+        
+        // vertical movement
         if (keys.down.isDown) {
             sprite.y += playerSpeed;
         }
         if (keys.up.isDown) {
             sprite.y -= playerSpeed;
         }
+        // force the player to be within the game boundaries
+        if (sprite.y < 0) {
+            sprite.y = 0;
+        }
+        else if (sprite.y + sprite.height > game.height) {
+            sprite.y = game.height - sprite.height;
+        }
+        
+        // horizontal movement
         if (keys.right.isDown) {
             sprite.x += playerSpeed;
         }
         if (keys.left.isDown) {
             sprite.x -= playerSpeed;
+        }
+        // force the player to be within the game boundaries
+        if (sprite.x < 0) {
+            sprite.x = 0;
+        }
+        else if (sprite.x + sprite.width > game.width) {
+            sprite.x = game.width - sprite.width;
         }
     }
 }
