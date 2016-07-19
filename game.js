6a12887142c41346d3c09e7ac5e7fd522fde7bc8
Jezzamonn <jezzamonn@gmail.com>
2016-07-19 16:50:28 +1000

    Add scores and stuff

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1200, 700, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
 
-var playerSpeed = 5;
+var playerSpeed = 7;
 
 var players;
 var powerUp;
@@ -11,7 +11,7 @@ function preload() {
     game.load.image('player2', 'p2.png');
     game.load.image('player3', 'p3.png');
     game.load.image('player4', 'p4.png');
-    game.load.image('powerup', 'powerup.png');
+    game.load.image('powerup', 'EUlogo.png');
     
     keys = game.input.keyboard.createCursorKeys();
 }
@@ -26,7 +26,8 @@ function create() {
 }
 
 function initialisePowerUp() {
-    powerUp = game.add.sprite(0, 0, 'powerup')
+    powerUp = game.add.sprite(game.width / 2, game.height / 2, 'powerup')
+    powerUp.scale.setTo(0.1, 0.1);
 }
 
 function initialisePlayers() {
@@ -35,22 +36,23 @@ function initialisePlayers() {
     // This creates all the players with one big array literal.
     // Could bit made a little more modular, but on the whole it's ok.
     players = [
-        createPlayer('player1', ['UP', 'LEFT', 'DOWN', 'RIGHT']),
-        createPlayer('player2', ['W', 'A', 'S', 'D']),
-        createPlayer('player3', ['I', 'J', 'K', 'L']),
-        createPlayer('player4', ['T', 'F', 'G', 'H']),
+        createPlayer('player1', ['UP', 'LEFT', 'DOWN', 'RIGHT'], 'red', false, false),
+        createPlayer('player2', ['W', 'A', 'S', 'D'], 'green', false, true),
+        createPlayer('player3', ['I', 'J', 'K', 'L'], 'blue', true, false),
+        createPlayer('player4', ['T', 'F', 'G', 'H'], 'purple', true, true),
     ]
-    
-    // Space out the players
-    players[1].sprite.x = game.width - players[1].sprite.width;
-    players[2].sprite.y = game.height - players[2].sprite.height;
-    players[3].sprite.x = game.width - players[3].sprite.width;
-    players[3].sprite.y = game.height - players[3].sprite.height;
     
     //makePlayerMega(players[0]);
 }
 
-function createPlayer(spriteName, keyNames) {
+function createPlayer(spriteName, keyNames, color, upEdge, leftEdge) {
+    var fontStyle = { font: "20px Arial", fill: color };
+    if (leftEdge) {
+        fontStyle.align = "left";
+    }
+    else {
+        fontStyle.align = "right";
+    }
     player = {
         sprite: game.add.sprite(0, 0, spriteName),
         keys: game.input.keyboard.addKeys({
@@ -61,19 +63,42 @@ function createPlayer(spriteName, keyNames) {
         }),
         speedScale: 1.0,
         powerUp: null,
+        scoreText: game.add.text(0, 0, '0', fontStyle),
+        score: 0
+    }
+    if (upEdge) {
+        player.sprite.y = 0;
+        player.scoreText.y = 0;
+    }
+    else {
+        player.sprite.y = game.height - player.sprite.height;
+        player.scoreText.y = game.height - player.scoreText.height;
+    }
+    if (leftEdge) {
+        player.sprite.x = 0;
+        player.scoreText.x = 0;
+    }
+    else {
+        player.sprite.x = game.width - player.sprite.width;
+        player.scoreText.x = game.width - player.scoreText.width;
+        player.scoreText.anchor.x = 1;
     }
     makePlayerRegular(player)
     return player;
 }
 
+function updateScore(player) {
+    player.scoreText.setText(player.score);
+}
+
 function makePlayerRegular(player) {
-    changePlayerScale(player, 0.2);
+    changePlayerScale(player, 0.25);
     player.speedScale = 1.0;
     player.powerUp = null;
 }
 
 function makePlayerMega(player) {
-    changePlayerScale(player, 0.4);
+    changePlayerScale(player, 0.5);
     player.speedScale = 1.1;
     player.powerUp = "mega";
 }
@@ -113,7 +138,10 @@ function spritesTouching(sprite1, sprite2) {
 }
 
 function update() {
+    powerUp.tint = Math.floor(0xFFFFFF * Math.random())
+
     var j;
+    // update players
     for (var i = 0; i < players.length; i ++) {
         player = players[i];
         sprite = players[i].sprite;
@@ -162,6 +190,10 @@ function update() {
                 // will be moved so that they still touch. If this happens, it'll
                 // try again.
                 if (spritesTouching(player.sprite, players[j].sprite)) {
+                    // We've just attacked a player.
+                    player.score += 73
+                    updateScore(player)
+
                     while (spritesTouching(player.sprite, players[j].sprite) || spritesTouching(powerUp, players[j].sprite)) {
                         players[j].sprite.x = (game.width - players[j].sprite.width) * Math.random();
                         players[j].sprite.y = (game.height - players[j].sprite.height) * Math.random();
